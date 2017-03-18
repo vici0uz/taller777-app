0e2cd274923b8cb28ddd9cf82099fa64a276731b
vici0uz <sasorinocho@mixmail.com>
2017-03-18 09:47:30 -0300

    Agregado Caja Chica a Taller777 Odoo Mobile

--- app/src/main/java/com/odoo/addons/caja_chica/CajaDetails.java
+++ app/src/main/java/com/odoo/addons/caja_chica/CajaDetails.java
@@ -1,15 +1,40 @@
 package com.odoo.addons.caja_chica;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TableLayout;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.odoo.R;
 import com.odoo.addons.caja_chica.models.AccountBankStatement;
 import com.odoo.core.orm.ODataRow;
 import com.odoo.core.orm.fields.OColumn;
 import com.odoo.core.support.OdooCompatActivity;
+import com.odoo.core.utils.JSONUtils;
+import com.odoo.core.utils.StringUtils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import odoo.controls.OField;
 import odoo.controls.OForm;
 
 /**
@@ -35,11 +60,13 @@ public class CajaDetails extends OdooCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         extras = getIntent().getExtras();
 
         accountBankStatement = new AccountBankStatement(this, null);
         setupToolbar();
-
     }
 
     private void setupToolbar(){
@@ -47,5 +74,44 @@ public class CajaDetails extends OdooCompatActivity {
         record = accountBankStatement.browse(rowId);
         mForm = (OForm) findViewById(R.id.cajaForm);
         mForm.initForm(record);
+        collapsingToolbarLayout.setTitle(record.getString("name"));
+        setupList();
+    }
+
+    private void setupList(){
+        List lines = record.getO2MRecord("line_ids").browseEach();
+        ArrayList<String> concepto = new ArrayList<String>();
+        ArrayList<String> monto = new ArrayList<String>();
+        ArrayList<String> date = new ArrayList<String>();
+        for(int i= 0; i<lines.size(); i++){
+            String l = lines.get(i).toString();
+//            NAME
+            Pattern patternName = Pattern.compile("name=(.*?),");
+            Matcher matcherName = patternName.matcher(l);
+            if (matcherName.find())
+                concepto.add(matcherName.group(1));
+//            MONTO
+            Pattern patternMount = Pattern.compile("mount=(.*?),");
+            Matcher matcherMount = patternMount.matcher(l);
+            if(matcherMount.find())
+                monto.add(matcherMount.group(1));
+//            Fecha
+            Pattern patternDate = Pattern.compile("\\sdate=(.*?),");
+            Matcher matcherDate = patternDate.matcher(l);
+            if(matcherDate.find())
+                date.add(matcherDate.group(1));
+
+        }
+        for(String p: concepto)
+            System.out.println(p);
+        Tabla tabla = new Tabla(this, (TableLayout)findViewById(R.id.tabla));
+        tabla.agregarCabecera(R.array.cabecera_tabla);
+        for(int x=0; x<concepto.size(); x++){
+            ArrayList<String> elementos = new ArrayList<String>();
+            elementos.add(concepto.get(x));
+            elementos.add(monto.get(x));
+            elementos.add(date.get(x));
+            tabla.agregarFilaTabla(elementos);
+        }
     }
 }
--- app/src/main/java/com/odoo/addons/caja_chica/models/AccountBankStatement.java
+++ app/src/main/java/com/odoo/addons/caja_chica/models/AccountBankStatement.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 
 import com.odoo.BuildConfig;
+import com.odoo.base.addons.res.ResPartner;
 import com.odoo.core.orm.OModel;
 import com.odoo.core.orm.fields.OColumn;
 import com.odoo.core.orm.fields.types.ODate;
+import com.odoo.core.orm.fields.types.OFloat;
 import com.odoo.core.orm.fields.types.OSelection;
 import com.odoo.core.orm.fields.types.OVarchar;
 import com.odoo.core.support.OUser;
@@ -25,6 +27,14 @@ public class AccountBankStatement extends OModel{
                 .addSelection("confirm", "Closed");
         OColumn date = new OColumn("Date", ODate.class);
         OColumn line_ids = new OColumn("Records", AccountBankStatementLine.class, OColumn.RelationType.OneToMany).setRelatedColumn("statement_id");
+
+        OColumn user_id = new OColumn("User", ResPartner.class, OColumn.RelationType.ManyToOne);
+        OColumn balance_start = new OColumn("Saldo de Apertura", OFloat.class);
+        OColumn total_entry_encoding = new OColumn("Transacciones", OFloat.class);
+        OColumn balance_end = new OColumn("Saldo Teorico", OFloat.class);
+        OColumn balance_end_real = new OColumn("Saldo Real", OFloat.class);
+        OColumn difference = new OColumn("Diferencia", OFloat.class);
+
 
     public AccountBankStatement(Context context, OUser user) {
         super(context, "account.bank.statement", user);
